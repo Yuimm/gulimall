69c2803b9fc21a71ea7a77b4b63a4247421f4748
Yuimm <974288521@qq.com>
2021-07-26 15:55:35 +0800

    项目初始化创建

---
+++ gulimall-ware/src/main/java/com/hct/gulimall/ware/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.hct.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
