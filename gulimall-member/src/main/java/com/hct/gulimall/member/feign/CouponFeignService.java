77c1dfc472244e915e447edb41c610647658f29a
Yuimm <974288521@qq.com>
2021-07-26 19:34:01 +0800

    配置Nacos注册中心、member与coupon之间的feign调用

---
+++ gulimall-member/src/main/java/com/hct/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,12 @@
+package com.hct.gulimall.member.feign;
+
+import com.hct.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
